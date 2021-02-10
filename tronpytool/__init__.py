3825b0995d7f6f82b02b624d3cc4104b3a5eca13
heskdx <jobhesk@gmail.com>
2021-02-10 20:37:40 +0800

    package updates related items. please check in commit details

--- tronpytool/__init__.py
+++ tronpytool/__init__.py
@@ -10,15 +10,15 @@ import sys
 import pkg_resources
 from eth_account import Account  # noqa: E402
 
-from compile.basecore import ContractMethod
-from compile.basetest import SolcWrap
-from compile.service import Service, BackgroundRun
-from pen.collectfundscn import Collector
-from pen.contracttest import CoreSimulatePlayers
-from pen.eventtest import EventTestCase
-from pen.membertest import MemberNetworkTestCase
-from pen.membertest import MemberNetworkTestCase
-from providers.gracefulinterrupter import GracefulInterruptHandler
+from .compile.basecore import ContractMethod
+from .compile.basetest import SolcWrap
+from .compile.service import Service, BackgroundRun
+from .pen.collectfundscn import Collector
+from .pen.contracttest import CoreSimulatePlayers
+from .pen.eventtest import EventTestCase
+from .pen.membertest import MemberNetworkTestCase
+from .pen.membertest import MemberNetworkTestCase
+from .providers.gracefulinterrupter import GracefulInterruptHandler
 from .main import Tron  # noqa: E402
 from .providers.http import HttpProvider  # noqa: E402
 
