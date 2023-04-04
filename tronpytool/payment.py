5dc4c7537ad4d0171f88840950fe91f1b27476c0
heskdx <jobhesk@gmail.com>
2023-04-04 12:43:22 +0800

    ♻️ update network notifications  3 files changed, 4 insertions(+), 4 deletions(-)

--- tronpytool/payment.py
+++ tronpytool/payment.py
@@ -23,7 +23,7 @@ class Key:
 
 
 def PrintNetworkName(tron):
-    print(f"You are now using network {Bolors.WARNING}{str(tron.network_name).upper()}{Bolors.RESET}")
+    print(f"Now using TRON network in {Bolors.WARNING}{str(tron.network_name).upper()}{Bolors.RESET}")
 
 
 class Payment(CoreDeploy):
